2e0cc568d279a5376e480fdd95bb2b66c71ee498
seiyahawegawa <h-ayiesn_n-1270@docomo.ne.jp>
2020-12-14 14:48:42 +0900

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,22 @@
 class ApplicationController < ActionController::Base
  
-  before_action :basic_auth
-  before_action :basic_auth, if: :production?
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!, except: [:index, :show] 
-  private
+  # before_action :basic_auth
+  # before_action :basic_auth, if: :production?
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :authenticate_user!, except: [:index, :show] 
+  # private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :first_name, :family_name_kana, :first_name_kana])
 
   end
 
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == Rails.application.credentials[:basic_auth][:user] &&
-      password == Rails.application.credentials[:basic_auth][:pass]
-    end
-  end
-  def production?
-    Rails.env.production?
-  end
+  # def basic_auth
+  #   authenticate_or_request_with_http_basic do |username, password|
+  #     username == Rails.application.credentials[:basic_auth][:user] &&
+  #     password == Rails.application.credentials[:basic_auth][:pass]
+  #   end
+  # end
+  # def production?
+  #   Rails.env.production?
+  # end
 end
